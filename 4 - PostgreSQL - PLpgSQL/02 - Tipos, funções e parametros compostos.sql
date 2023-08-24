bb3d22ee05c6032dadd1528b04ed10fddefa4d32
LuisFelipeGM <lipe.garcia.menezes@gmail.com>
2023-08-24 10:15:24 -0300

    Add Script de Funções e Parametros compostos

--- 4 - PostgreSQL - PLpgSQL/02 - Tipos, funções e parametros compostos.sql
+++ 4 - PostgreSQL - PLpgSQL/02 - Tipos, funções e parametros compostos.sql
@@ -82,6 +82,46 @@ $$ language sql;
 
 -- Parâmetros de Saída
 
+-- Como vimos antes tem algumas formas de retornar mais de um valor da function, porem 
+-- tem uma outra forma de realizar isso atraves do (OUT) criando variaveis de saida
+
+create or replace function soma_e_produto (numero_1 integer, numero_2 integer, out soma integer, out produto integer) as $$
+	select numero_1 + numero_2 as soma, numero_1 * numero_2 as produto;
+$$ language sql;
+
+-- nessa função não temos o (returns) pois vamos retornar as variaveis de saida
+
+select * from soma_e_produto(3,3);
+
+-- Outra coisa que poderiamos fazer é criar um tipo e retornar esse tipo:
+
+create type dois_valores as (soma integer, produto integer);
+
+drop function soma_e_produto;
+
+create or replace function soma_e_produto (numero_1 integer, numero_2 integer)returns dois_valores as $$
+	select numero_1 + numero_2 as soma, numero_1 * numero_2 as produto;
+$$ language sql;
+
+select * from soma_e_produto(3,3);
+
+-- Sabendo desses parametros de saída agora podemos utilizar o record
+
+drop function instrutores_bem_pagos;
+
+create or replace function instrutores_bem_pagos(valor_salario decimal, out nome varchar, out salario decimal) returns setof record as $$
+	select nome, salario from instrutor where salario > valor_salario;
+$$ language sql;
+
+select * from instrutores_bem_pagos(300); 
+
+-- Como foi definido os parametros de saida anteriormente eu apenas preciso dizer que quero
+-- todos os registros (SETOF) e que retorna qualquer coisa (record), como já foi definido antes
+-- não é necessario definir de novo
+
+-- A boa prática é criar um tipo para deixar explicito o retorno, mas os parametro de saida são uma opção
+
+
 
 
 
